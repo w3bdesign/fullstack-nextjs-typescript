c32170c78ad23f583c072e5e4a49f7f6ca404aa8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-03 00:08:28 +0200

    We can now display images from Strapi

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,44 @@
+// https://nextjs.org/docs/basic-features/typescript
+
+import { useQuery, gql } from '@apollo/client';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const FETCH_RESTAURANTS = gql`
+  query GetRestaurants {
+    restaurants {
+      id
+      name
+      description
+      image {
+        url
+      }
+    }
+  }
+`;
+
+const Index = () => {
+  const { loading, error, data } = useQuery(FETCH_RESTAURANTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
+
+  //return <h1>Graphql see console.log</h1>;
+
+  return data.restaurants.map(({ id, name, description, image }) => (
+    <Row key={id}>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={`http://localhost:1338${image.url}`} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button variant="primary">Text</Button>
+        </Card.Body>
+      </Card>
+    </Row>
+  ));
+};
+
+export default Index;
