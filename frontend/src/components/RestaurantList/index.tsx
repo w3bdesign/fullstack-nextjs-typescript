27476d038a85880ff3d81b6637b8aff39e9235a2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-03 02:50:49 +0200

    Fix eslint proptype error

--- frontend/src/components/RestaurantList/index.tsx
+++ frontend/src/components/RestaurantList/index.tsx
@@ -4,10 +4,11 @@ import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 import LIST_RESTAURANTS from '../../gql/LIST_RESTAURANTS';
 
-type TRestaurant = {
+export type TRestaurant = {
   id: number;
   name: string;
   description: string;
@@ -33,26 +34,35 @@ const RestaurantList = () => {
     );
   }
 
-  return data.restaurants.map(
-    ({
-      id, name, description, image,
-    }: TRestaurant) => (
-      <Container>
-        <Row key={id} className="justify-content-md-center">
-          <Card style={{ width: '18rem', margin: '2rem' }}>
-            <Card.Img
-              variant="top"
-              src={`${process.env.NEXT_PUBLIC_API_URL}${image.url}`}
-            />
-            <Card.Body>
-              <Card.Title>{name}</Card.Title>
-              <Card.Text>{description}</Card.Text>
-              <Button variant="primary">Text</Button>
-            </Card.Body>
-          </Card>
-        </Row>
-      </Container>
-    ),
+  return (
+    <>
+      <Form>
+        <Form.Group controlId="formBasicInput">
+          <Form.Label>Search restaurants</Form.Label>
+          <Form.Control type="text" placeholder="Enter restaurant name" style={{ width: '20rem', margin: '2rem' }} className="justify-content-md-center" />
+        </Form.Group>
+      </Form>
+
+      {data.restaurants.map(({
+        id, name, description, image,
+      }: TRestaurant) => (
+        <Container>
+          <Row key={id} className="justify-content-md-center">
+            <Card style={{ width: '18rem', margin: '2rem' }}>
+              <Card.Img
+                variant="top"
+                src={`${process.env.NEXT_PUBLIC_API_URL}${image.url}`}
+              />
+              <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+                <Button variant="primary">Text</Button>
+              </Card.Body>
+            </Card>
+          </Row>
+        </Container>
+      ))}
+    </>
   );
 };
 
