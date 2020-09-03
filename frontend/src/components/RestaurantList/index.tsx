c71b8f20c1b878eec1ecb37dd0ecbdaab68128e0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-03 03:13:25 +0200

    Separate search into a separate component

--- frontend/src/components/RestaurantList/index.tsx
+++ frontend/src/components/RestaurantList/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
 
 import LIST_RESTAURANTS from '../../gql/LIST_RESTAURANTS';
 
@@ -38,37 +36,26 @@ const RestaurantList = () => {
   return (
     <>
       <Container>
-        <Row className="justify-content-md-center" style={{ marginTop: '1rem' }}>
 
-          <Form>
-            <Form.Group controlId="formBasicInput">
-              <Form.Label>Search restaurants</Form.Label>
-              <Form.Control type="text" placeholder="Enter restaurant name" style={{ width: '18rem', marginTop: '1rem' }} />
-              <Button variant="primary" style={{ marginTop: '1rem' }}>Search</Button>
-            </Form.Group>
-          </Form>
-
-        </Row>
-
-        {data.restaurants.map(({
-          id, name, description, image,
-        }: TRestaurant) => (
-
-          <Row key={id} className="justify-content-md-center">
-            <Card style={{ width: '18rem', margin: '2rem' }}>
-              <Card.Img
-                variant="top"
-                src={`${process.env.NEXT_PUBLIC_API_URL}${image.url}`}
-              />
-              <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>{description}</Card.Text>
-                <Button variant="primary">Text</Button>
-              </Card.Body>
-            </Card>
-          </Row>
-
-        ))}
+        {data.restaurants.map(
+          ({
+            id, name, description, image,
+          }: TRestaurant) => (
+            <Row key={id} className="justify-content-md-center">
+              <Card style={{ width: '18rem', margin: '2rem' }}>
+                <Card.Img
+                  variant="top"
+                  src={`${process.env.NEXT_PUBLIC_API_URL}${image.url}`}
+                />
+                <Card.Body>
+                  <Card.Title>{name}</Card.Title>
+                  <Card.Text>{description}</Card.Text>
+                  <Button variant="primary">Text</Button>
+                </Card.Body>
+              </Card>
+            </Row>
+          ),
+        )}
       </Container>
     </>
   );
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 // https://nextjs.org/docs/basic-features/typescript
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import RestaurantList from '../components/RestaurantList/index';
+import SearchInput from '../components/SearchInput/index';
 
-const Index = () => (
-  // const [query, updateQuery] = useState('');
+const Index = () => {
+  const [query, updateQuery] = useState('');
 
-  <RestaurantList />
-);
+  return (
+    <>
+      <SearchInput query={query} updateQuery={updateQuery} />
+      <RestaurantList />
+    </>
+  );
+};
 export default Index;
