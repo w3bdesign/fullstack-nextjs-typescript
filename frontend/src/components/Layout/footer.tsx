da51805809da5026efb689b7e082d41c66378393
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-06 02:06:58 +0200

    Add footer and center text

--- frontend/src/components/Layout/footer.tsx
+++ frontend/src/components/Layout/footer.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
-const Footer = () => <h1>Footer</h1>;
+const Footer = () => (
+  <Navbar bg="secondary" variant="dark">
+    <Container className="text-center justify-content-md-center">
+      <Navbar.Brand>Footer comes here</Navbar.Brand>
+    </Container>
+  </Navbar>
+);
 
 export default Footer;
