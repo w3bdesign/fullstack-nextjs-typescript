72ea3f223821abef364f48ba831e1b595da83671
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-06 03:50:25 +0200

    Install Axios and begin setting up authentication

---
+++ frontend/src/lib/Auth.ts
@@ -0,0 +1,3 @@
+// https://strapi.io/documentation/3.0.0-beta.x/guides/auth-request.html#setup
+// https://strapi.io/blog/nextjs-react-hooks-strapi-auth-4
+// https://flaviocopes.com/nextjs-cookies/
