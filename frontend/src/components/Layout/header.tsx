0cc39acddd5d2f75e1707e03a666215091e608c4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-06 03:34:24 +0200

    Setup header to use next/link

--- frontend/src/components/Layout/header.tsx
+++ frontend/src/components/Layout/header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
+
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 const Header = () => (
   <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="/">Food order fullstack application</Navbar.Brand>
+    <Navbar.Brand>Food order fullstack application</Navbar.Brand>
     <Nav className="justify-content-center" activeKey="/">
-      <Nav.Link href="/">Restaurants</Nav.Link>
+      <Link href="/">
+        <Nav.Link>Restaurants</Nav.Link>
+      </Link>
     </Nav>
   </Navbar>
 );
