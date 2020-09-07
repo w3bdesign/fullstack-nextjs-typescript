7dca613a125c13853a56606de6a433dfdf4952ae
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-07 03:53:55 +0200

    Fix header and footer color and placement

--- frontend/src/components/Layout/header.tsx
+++ frontend/src/components/Layout/header.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
-const Header = () => (
-  <Navbar bg="primary" variant="dark">
-    <Navbar.Brand>Food order fullstack application</Navbar.Brand>
-    <Nav className="justify-content-center">
-      <Link href="/">
-        <Nav.Link href="/">Restaurants</Nav.Link>
-      </Link>
-      <Link href="/login">
-        <Nav.Link href="/login">Login</Nav.Link>
-      </Link>
-      <Link href="/register">
-        <Nav.Link href="/register">Register</Nav.Link>
-      </Link>
-    </Nav>
-    <Nav className="justify-content-center">
-      User:
-    </Nav>
+import AppContext from '../../context/AppContext';
 
-  </Navbar>
-);
+const Header = () => {
+  const [user, setUser] = useContext(AppContext);
+
+  return (
+    <Navbar bg="dark" variant="dark" className="justify-content-lg-around">
+      <Navbar.Brand>Food order fullstack application</Navbar.Brand>
+      <Nav className="justify-content-center">
+        <Link href="/">
+          <Nav.Link href="/">Restaurants</Nav.Link>
+        </Link>
+        <Link href="/login">
+          <Nav.Link href="/login">Login</Nav.Link>
+        </Link>
+        <Link href="/register">
+          <Nav.Link href="/register">Register</Nav.Link>
+        </Link>
+      </Nav>
+      <Navbar.Text className="mr-sm-2">
+        User:
+        {user}
+      </Navbar.Text>
+    </Navbar>
+  );
+};
 
 export default Header;
